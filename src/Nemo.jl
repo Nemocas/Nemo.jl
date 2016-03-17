2380534d2c21d1a6909e2dc438e10b900a1ac960
Tommy Hofmann <thofma@gmail.com>
2016-03-17 14:30:12 +0100

    Bla

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -249,6 +249,10 @@ Ideal = PariIdeal
 #
 ###############################################################################
 
+println("libarb: $(Libdl.find_library(["libarb"]))")
+println("libflint: $(Libdl.find_library(["libflint"]))")
+println("libpari: $(Libdl.find_library(["libpari"]))")
+
 include("../test/Nemo-test.jl")
 
 include("../benchmarks/runbenchmarks.jl")
