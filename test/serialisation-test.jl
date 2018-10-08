e76c6f39cf0e5e2efc061072c357d8252bfc8603
Syed Mehran Hussain <s8syhuss@stud.uni-saarland.de>
2018-10-08 14:40:33 +0200

    serialisation code extended

--- test/serialisation-test.jl
+++ test/serialisation-test.jl
@@ -1,37 +1,22 @@
-abstract type Hello end
- 
-struct HelloWorld <: Hello
-    name::String
-    HelloWorld(s) = new(s)
+function serialise(sValue)
+	
+	io = open("objects.dat", "w");
+	write(io, sValue);
+	close(io);
+	return 0
 end
- 
-struct HelloTime <: Hello
-    name::String
-    tnew::DateTime
-    HelloTime(s) = new(s, now())
+
+function deSerialise()
+	
+	io = open("objects.dat", "r");
+	dValue = read(io, String);
+	close(io);
+	return dValue
 end
+
+
+serialise("sample data");
  
-sayhello(hlo) = println("Hello to this world, $(hlo.name)!")
+sleep(10);
  
-sayhello(hlo::HelloTime) = println("It is now $(now()). Hello from back in $(hlo.tnew), $(hlo.name)!")
- 
-h1 = HelloWorld("world")
-h2 = HelloTime("new world")
- 
-sayhello(h1)
-sayhello(h2)
- 
-fh = open("objects.dat", "w")
-serialize(fh, h1)
-serialize(fh,h2)
-close(fh)
- 
-sleep(10)
- 
-fh = open("objects.dat", "r")
-hh1 = deserialize(fh)
-hh2 = deserialize(fh)
-close(fh)
- 
-sayhello(hh1)
-sayhello(hh2)
+deSerialise()
