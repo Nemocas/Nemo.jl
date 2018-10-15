08d3a8828fea31981a5c49bb5ebe27097f69915a
Syed Mehran Hussain <s8syhuss@stud.uni-saarland.de>
2018-10-15 12:55:29 +0200

    serialisation source file created

---
+++ src/serialization.jl
@@ -0,0 +1,25 @@
+function serialise(sValue)
+	
+	io = open("objects.dat", "w");
+	write(io, sValue);
+	close(io);
+	return 0
+end
+
+function deSerialise(dType)
+	
+	io = open("objects.dat", "r");
+	dValue = read(io, dType);
+	close(io);
+	return dValue
+end
+
+data = "sample data";
+serialise(data);
+ 
+sleep(5);
+ 
+deSerialise(typeof(data))
+
+
+
