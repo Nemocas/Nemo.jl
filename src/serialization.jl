48a85c19a9be2ace1f95b5c592b675716acd78e1
Syed Mehran Hussain <s8syhuss@stud.uni-saarland.de>
2018-10-15 13:32:06 +0200

    fmpz type serialization

--- src/serialization.jl
+++ src/serialization.jl
@@ -1,3 +1,30 @@
+#Flint type serialization
+
+
+function serialize(s::AbstractSerializer, t::fmpz)
+  return serialize(s, base(t, 62))
+end
+
+function deserialize(s::AbstractSerializer, t::Type{fmpz})
+  return parse(fmpz, deserialize(s), 62)
+end
+
+function serialize(s::AbstractSerializer, t::fmpq)
+  serialize_type(s, fmpq)
+  serialize(s, base(numerator(t), 62))
+  return serialize(s, base(denominator(t), 62))
+end
+
+function deserialize(s::AbstractSerializer, t::Type{fmpq})
+  n = parse(fmpz, deserialize(s), 62)
+  d = parse(fmpz, deserialize(s), 62)
+  return fmpq(n, d)
+end
+
+
+
+
+# simple I/O custom serialization
 function serialise(sValue)
 	
 	io = open("objects.dat", "w");
