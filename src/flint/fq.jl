d6780c439c62a44bb5c021cfae7a4b60d9046446
Daniel Schultz <tthsqe12@gmail.com>
2020-12-01 15:12:05 +0100

    fix the doc fixes

--- src/flint/fq.jl
+++ src/flint/fq.jl
@@ -492,8 +492,8 @@ end
 @doc Markdown.doc"""
     frobenius(x::fq, n = 1)
 
-Return the iterated Frobenius $\sigma_p^n(a)$ where $\sigma_p$ is the
-Frobenius map sending the element $x$ to $x^p$ in the finite field of
+Return the iterated Frobenius $\sigma_p^n(x)$ where $\sigma_p$ is the
+Frobenius map sending the element $a$ to $a^p$ in the finite field of
 characteristic $p$. By default the Frobenius map is applied $n = 1$ times if
 $n$ is not specified.
 """
