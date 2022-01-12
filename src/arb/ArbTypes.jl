59ad48a2b7e1817626a701dc017b33bb8b87bc25
Max Horn <max@quendi.de>
2022-01-13 00:59:17 +0100

    Remove two superfluous elem_type methods

--- src/arb/ArbTypes.jl
+++ src/arb/ArbTypes.jl
@@ -355,8 +355,6 @@ end
 
 parent(x::arb_poly) = x.parent
 
-elem_type(x::ArbPolyRing) = arb_poly
-
 var(x::ArbPolyRing) = x.S
 
 precision(x::ArbPolyRing) = precision(x.base_ring)
@@ -473,8 +471,6 @@ function _acb_poly_clear_fn(x::acb_poly)
 end
 
 parent(x::acb_poly) = x.parent
-
-elem_type(x::AcbPolyRing) = acb_poly
 
 var(x::AcbPolyRing) = x.S
 
