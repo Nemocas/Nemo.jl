b6125727e0b192f29177914d2af7e840bc1a3d22
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-22 17:05:41 +0200

    Add `test/Project.toml`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Nemo
 using Test
-using InteractiveUtils
+using Nemo.InteractiveUtils
 
 import Nemo.AbstractAlgebra
 include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
