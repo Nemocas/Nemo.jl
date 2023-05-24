c3b629db0779a46395134825a2de968a5894440c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-24 12:35:46 +0200

    Remove `InteractiveUtils` as dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Nemo
 using Test
-using Nemo.InteractiveUtils
+using InteractiveUtils: @which
 
 import Nemo.AbstractAlgebra
 include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
