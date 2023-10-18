47565aa98755c3017bd1325b784ecd0d207a880d
Max Horn <max@quendi.de>
2023-10-18 23:01:22 +0200

    Restrict round signature to avoid ambiguities

--- test/flint/fmpq-test.jl
+++ test/flint/fmpq-test.jl
@@ -185,9 +185,15 @@ end
    @test trunc(QQFieldElem(-1, 3)) == 0
    @test trunc(QQFieldElem(2, 1)) == 2
 
+   vals = vcat(
+        Any[d//3 for d in -15:15],
+        Any[Rational{BigInt}(d//3) for d in -15:15],
+        Any[Rational{Int16}(d//3) for d in -15:15],
+        Any[true//true, false//true],
+    )
+
    @testset "$func" for func in (trunc, round, ceil, floor)
-      for d in -15:15
-         val = d//3
+      for val in vals
          valQ = QQFieldElem(val)
          @test func(valQ) isa QQFieldElem
          @test func(valQ) == func(val)
@@ -207,8 +213,7 @@ end
    end
 
    @testset "$mode" for mode in (RoundUp, RoundDown, RoundNearest, RoundNearestTiesAway)
-      for d in -5:5
-         val = d//3
+      for val in vals
          valQ = QQFieldElem(val)
          @test round(valQ, mode) isa QQFieldElem
          @test round(valQ, mode) == round(val, mode)
@@ -217,7 +222,7 @@ end
          @test round(QQFieldElem, valQ, mode) == round(val, mode)
 
          @test round(ZZRingElem, valQ, mode) isa ZZRingElem
-         @test round(ZZRingElem, valQ, mode) == round(val, mode)
+         @test round(ZZRingElem, valQ, mode) == QQFieldElem(round(val, mode)) # HACK to work around missing adhoc comparision
 
          @test round(BigInt, valQ, mode) isa BigInt
          @test round(BigInt, valQ, mode) == round(val, mode)
