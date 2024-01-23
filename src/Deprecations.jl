87ffc41e283d4500841a2a5ee8517d54583dce12
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-01-23 10:09:09 +0100

    Update src/Deprecations.jl

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -118,5 +118,6 @@
 
 # Deprecated in 0.39.*
 
+@deprecate divisible(x::Int, y::Int) is_divisible_by(x, y)
 @deprecate divisible(x::ZZRingElem, y::Int) is_divisible_by(x, y)
 @deprecate divisible(x::ZZRingElem, y::ZZRingElem) is_divisible_by(x, y)
