e1b1918e46537fdfa4de0309e6b5e211d54fe066
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-01-23 15:23:46 +0100

    Implement new linear solving interface for `QQMatrix` [no ci]

--- src/flint/fmpq_mat.jl
+++ src/flint/fmpq_mat.jl
@@ -704,6 +704,32 @@ function can_solve(a::QQMatrix, b::QQMatrix; side::Symbol = :right)
    return fl
 end
 
+function AbstractAlgebra.Solve._can_solve_internal(A::QQMatrix, b::QQMatrix, task::Symbol; side = Symbol = :right)
+   if task !== :only_check && task !== :with_solution && task !== :with_kernel
+      error("task $(task) not recognized")
+   end
+
+   if side !== :right && side !== :left
+      throw(ArgumentError("Unsupported argument :$side for side: Must be :left or :right."))
+   end
+
+   if side === :left
+      fl, sol, K = AbstractAlgebra.Solve._can_solve_internal(transpose(A), transpose(b), task, side = :right)
+      return fl, transpose(sol), transpose(K)
+   end
+
+   nrows(A) != nrows(b) && error("Incompatible matrices")
+
+   x = similar(A, ncols(A), ncols(b))
+   fl = ccall((:fmpq_mat_can_solve_multi_mod, libflint), Cint,
+              (Ref{QQMatrix}, Ref{QQMatrix}, Ref{QQMatrix}), x, A, b)
+
+   if task === :only_check || task === :with_solution
+      return Bool(fl), x, zero(A, 0, 0)
+   end
+   return Bool(fl), x, kernel(A)[2]
+end
+
 ###############################################################################
 #
 #   Trace
