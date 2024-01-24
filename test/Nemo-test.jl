abf60452ab1f8b0287aac8e5cf7901f69df8e3dd
Aaruni Kaushik <aaruni@edufor.me>
2024-01-24 14:50:19 +0100

    Adds parallel testing
    Saves maybe 50 seconds using 10 workers. Is it even worth it ?

--- test/Nemo-test.jl
+++ test/Nemo-test.jl
@@ -1,12 +1,15 @@
-include("Fields-test.jl")
-include("Rings-test.jl")
-include("Generic-test.jl")
-include("Benchmark-test.jl")
-include("gaussiannumbers/continued_fraction-test.jl")
-include("Native-test.jl")
-include("Infinity-test.jl")
-include("HeckeMiscLocalization-test.jl")
-include("matrix-test.jl")
-include("poly-test.jl")
-include("ZZMatrix-linalg-test.jl")
+testlist = [
+    "Fields-test.jl",
+    "Rings-test.jl",
+    "Generic-test.jl",
+    "Benchmark-test.jl",
+    "gaussiannumbers/continued_fraction-test.jl",
+    "Native-test.jl",
+    "Infinity-test.jl",
+    "HeckeMiscLocalization-test.jl",
+    "matrix-test.jl",
+    "poly-test.jl",
+    "ZZMatrix-linalg-test.jl"
+]
 
+pmap(include, testlist)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,30 @@
 using Nemo
-using Test
-using InteractiveUtils: @which
 
-import Nemo.AbstractAlgebra
-include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
+using Distributed
+
+numprocs_str = get(ENV, "NUMPROCS", "1")
+
+if !isempty(ARGS)
+    jargs = [arg for arg in ARGS if startswith(arg, "-j")]
+    if !isempty(jargs)
+        numprocs_str = split(jargs[end], "-j")[end]
+    end
+end
+
+const numprocs = parse(Int, numprocs_str)
+
+if numprocs >= 2
+    println("Adding worker processes")
+    addprocs(numprocs)
+end
+
+@everywhere using Nemo
+@everywhere using Test
+@everywhere using InteractiveUtils: @which
+
+@everywhere import Nemo.AbstractAlgebra
+@everywhere include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
 
 include("Aqua.jl")
-include("rand.jl")
+@everywhere include("rand.jl")
 include("Nemo-test.jl")
