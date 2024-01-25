cae800c23b818a8f184f7fd3a101d1f5d09bb155
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-01-25 10:00:40 +0100

    Increase coverage

--- test/flint/gfp_mat-test.jl
+++ test/flint/gfp_mat-test.jl
@@ -711,10 +711,12 @@ end
       A = rand(M)
       B = rand(N)
 
-      fl, X = AbstractAlgebra.Solve.can_solve_with_solution(A, B)
+      fl, X, K = AbstractAlgebra.Solve.can_solve_with_solution_and_kernel(A, B)
 
       if fl
          @test A * X == B
+         @test is_zero(A*K)
+         @test rank(A) + ncols(K) == ncols(A)
       end
    end
 
