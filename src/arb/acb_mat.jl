600a65257a437b909c464199cc2278721fbd3c08
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-01-25 15:41:12 +0100

    `_can_solve_internal_no_check` for `acb_mat` [no ci]

--- src/arb/acb_mat.jl
+++ src/arb/acb_mat.jl
@@ -600,6 +600,25 @@ function solve_lu_precomp(P::Generic.Perm, LU::AcbMatrix, y::AcbMatrix)
   return z
 end
 
+function AbstractAlgebra.Solve._can_solve_internal_no_check(A::acb_mat, b::acb_mat, task::Symbol; side::Symbol = :right)
+   nrows(A) != ncols(A) && error("Only implemented for square matrices")
+   if side === :left
+      fl, sol, K = AbstractAlgebra.Solve._can_solve_internal_no_check(transpose(A), transpose(b), task, side = :right)
+      return fl, transpose(sol), transpose(K)
+   end
+
+   x = similar(A, ncols(A), ncols(b))
+   fl = ccall((:acb_mat_solve, libarb), Cint,
+              (Ref{acb_mat}, Ref{acb_mat}, Ref{acb_mat}, Int),
+              x, A, b, precision(base_ring(A)))
+   fl == 0 && error("Matrix cannot be inverted numerically")
+   if task === :only_check || task === :with_solution
+      return true, x, zero(A, 0, 0)
+   end
+   # If we ended up here, then A is invertible, so the kernel is trivial
+   return true, x, zero(A, ncols(A), 0)
+end
+
 ################################################################################
 #
 #   Row swapping
