749bbbb3842502edc5ad7c71c49e22172f087b43
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-05-14 19:07:35 +0200

    Re-add broken `(::fqPolyRepField)(::Vector)`

--- src/HeckeMiscFiniteField.jl
+++ src/HeckeMiscFiniteField.jl
@@ -1,4 +1,6 @@
-function (k::fqPolyRepField)(a::Vector)
+# As of 14 May 2024:
+# This is used in OSCAR, but requires (::fqPolyRepField)(::fpPolyRingElem)
+# which is in Hecke
+function (k::fqPolyRepField)(a::Vector{fpFieldElem})
   return k(polynomial(Native.GF(Int(characteristic(k))), a))
 end
-
