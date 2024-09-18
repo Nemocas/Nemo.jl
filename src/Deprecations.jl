b6d14fe96e3c6989fb2661d0c67c4e14bc03d3cb
Lars Göttgens <lars.goettgens@gmail.com>
2024-09-18 16:31:24 +0200

    Update src/Deprecations.jl

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -90,5 +90,6 @@ end
 @deprecate is_power(x::QQFieldElem) is_perfect_power_with_data(x)
 @deprecate is_power(x::Rational) is_perfect_power_with_data(x)
 
+# Deprecated in 0.47
 @deprecate coefficient_ring(k::PadicField) base_field(k)
 @deprecate coefficient_ring(k::QadicField) base_field(k)
