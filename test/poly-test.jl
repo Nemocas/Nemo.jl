7693250f78e83ad6c8e4b3b601a5d98a09663c5b
Johannes Schmitt <jschmitt@posteo.eu>
2024-09-18 17:06:14 +0200

    Move `is_squarefree`

--- test/poly-test.jl
+++ test/poly-test.jl
@@ -8,3 +8,21 @@
   #@test all(is_squarefree, keys(sf.fac))
   @test evaluate(sf) == f
 end
+
+@testset "Squarefreeness" begin
+  R, x = QQ["x"]
+  @test !is_squarefree(R(0))
+  @test is_squarefree(R(3))
+  @test is_squarefree(x)
+  @test !is_squarefree(x^2)
+  @test is_squarefree(2*x)
+  @test !is_squarefree(2*x^2)
+
+  R, x = ZZ["x"]
+  @test !is_squarefree(R(0))
+  @test is_squarefree(R(3))
+  @test is_squarefree(x)
+  @test !is_squarefree(x^2)
+  @test is_squarefree(2*x)
+  @test !is_squarefree(2*x^2)
+end
