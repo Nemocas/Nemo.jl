31370e8e15c21d521501c5ea11fd30cd3b87fdcd
Johannes Schmitt <jschmitt@posteo.eu>
2024-09-18 08:55:43 +0200

    Remove redundant `^(::ResElem, ::ZZRingElem)`

--- src/HeckeMoreStuff.jl
+++ src/HeckeMoreStuff.jl
@@ -677,24 +677,6 @@ function divexact(a::ZZModRingElem, y::ZZRingElem; check::Bool=true)
   return divexact(a, parent(a)(y), check=check)
 end
 
-function ^(a::ResElem, f::ZZRingElem)
-  f == 0 && return one(parent(a))
-  f == 1 && return a
-  if f < 0
-    f = -f
-    a = inv(a)
-  end
-  if f < (1 << 30)
-    return a^Int(f)
-  end
-  b = a^(div(f, 2))
-  b = b^2
-  if isodd(f)
-    b *= a
-  end
-  return b
-end
-
 characteristic(F::EuclideanRingResidueField{ZZRingElem}) = abs(F.modulus)
 
 #@doc raw"""
