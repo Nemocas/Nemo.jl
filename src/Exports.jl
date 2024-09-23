b83477fbdd44113ba0ae8fbd422b533b99d59835
Max Horn <max@quendi.de>
2024-09-23 23:22:46 +0200

    Add more QQFieldElem and ZZRingElem ccalls
    - add some missing add!, mul!, etc. methods
    - add more efficient +, -, * methods for a ZZRingElem or QQFieldElem
      as first argument, and an integer as second argument
    - optimize is_one and is_zero for QQFieldElem
    - drop some pointless uses of `@inline`
    - add `cmp` for QQFieldElem and use it
    - add numerator! and denominator!
    - change a bunch of "unsafe" methods to accept ZZRingElemOrPtr resp.
      QQFieldElemOrPtr instead of just ZZRingElem and QQFieldElem to
      make them more flexible

--- src/Exports.jl
+++ src/Exports.jl
@@ -137,6 +137,7 @@ export degree_fmpz
 export degrees
 export degrees_fit_int
 export degrees_fmpz
+export denominator!
 export derivative
 export det
 export det_divisor
@@ -464,6 +465,7 @@ export nullspace_right_rational
 export number_field
 export number_of_partitions
 export numpart
+export numerator!
 export oct
 export one
 export one!
