352e1918d3d2048fa7667a300861222ce9f7e1ee
Max Horn <max@quendi.de>
2024-09-24 21:47:46 +0200

    Update src/matrix.jl

--- src/matrix.jl
+++ src/matrix.jl
@@ -357,7 +357,7 @@ end
 @doc raw"""
     reduce_mod!(A::MatElem{T}, B::MatElem{T}) where T <: FieldElem
 
-For a reduced row echelon matrix $B$, reduce $A$ modulo $B$, i.e. all the pivot
+For a reduced row echelon matrix $B$, reduce the rows of $A$ modulo $B$, i.e. all the pivot
 columns will be zero afterwards.
 """
 function reduce_mod!(A::MatElem{T}, B::MatElem{T}) where {T<:FieldElem}
