d04ee2d01f6533027e6f47b8243710ac01421c7d
Martin Wagner <martin.wagner.dev@gmail.com>
2025-02-06 10:55:11 +0100

    Add more universal polynomial methods

--- src/UnivPoly.jl
+++ src/UnivPoly.jl
@@ -9,3 +9,27 @@
 # code. This mainly concerns rings over the rationals.
 
 denominator(f::UniversalPolyRingElem{QQFieldElem}) = denominator(data(f))
+
+function +(p::Generic.UnivPoly{T}, n::ZZRingElem) where {T}
+  S = parent(p)
+  return Generic.UnivPoly{T}(data(p)+n, S)
+end
+
++(n::ZZRingElem, p::Generic.UnivPoly) = p+n
+
+function -(p::Generic.UnivPoly{T}, n::ZZRingElem) where {T}
+  S = parent(p)
+  return Generic.UnivPoly{T}(data(p)-n, S)
+end
+
+function -(n::ZZRingElem, p::Generic.UnivPoly{T}) where {T}
+  S = parent(p)
+  return Generic.UnivPoly{T}(n-data(p), S)
+end
+
+function *(p::Generic.UnivPoly{T}, n::ZZRingElem) where {T}
+  S = parent(p)
+  return Generic.UnivPoly{T}(data(p)*n, S)
+end
+
+*(n::ZZRingElem, p::Generic.UnivPoly) = p*n
