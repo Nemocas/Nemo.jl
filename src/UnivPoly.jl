eb9dd6278d0abf7eee568f94785cb8b5aced27db
Martin Wagner <martin.wagner.dev@gmail.com>
2025-02-06 10:36:31 +0100

    Add an explaining comment to `UnivPoly.jl`

--- src/UnivPoly.jl
+++ src/UnivPoly.jl
@@ -1,1 +1,11 @@
+###############################################################################
+#
+#   Specialized methods for universal polynomials
+#
+###############################################################################
+
+# These methods speed up some computations with specific universal
+# polynomial rings which would otherwise be handled by more generic
+# code. This mainly concerns rings over the rationals.
+
 denominator(f::UniversalPolyRingElem{QQFieldElem}) = denominator(data(f))
