98ec37e2776106ee2053bc502d75b6878c06fe95
Claus Fieker <fieker@mathematik.uni-kl.de>
2025-02-21 18:34:59 +0100

    add inplace divides!

--- src/flint/fmpz.jl
+++ src/flint/fmpz.jl
@@ -425,10 +425,18 @@ function divexact(x::ZZRingElem, y::ZZRingElem; check::Bool=true)
   return z
 end
 
-function divides(x::ZZRingElem, y::ZZRingElem)
-  z = ZZRingElem()
+function divides!(z::ZZRingElem, x::ZZRingElem, y::ZZRingElem)
   res = @ccall libflint.fmpz_divides(z::Ref{ZZRingElem}, x::Ref{ZZRingElem}, y::Ref{ZZRingElem})::Bool
   return res, z
+end
+
+function divides!(z::T, x::T, y::T) where {T}
+  return AbstractAlgebra.divides(x, y)
+end
+
+function AbstractAlgebra.divides(x::ZZRingElem, y::ZZRingElem)
+  z = ZZRingElem()
+  return divides!(z, x, y)
 end
 
 divides(x::ZZRingElem, y::Integer) = divides(x, ZZRingElem(y))
