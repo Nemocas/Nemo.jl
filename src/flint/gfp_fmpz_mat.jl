85bbf10276c84c51b9d00e44bab089f5dc1e41d7
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-05-06 12:15:31 +0200

    Rename 5-arg `Base.view` methods

--- src/flint/gfp_fmpz_mat.jl
+++ src/flint/gfp_fmpz_mat.jl
@@ -138,7 +138,7 @@ end
 #
 ################################################################################
 
-function Base.view(x::FpMatrix, r1::Int, c1::Int, r2::Int, c2::Int)
+function _view_window(x::FpMatrix, r1::Int, c1::Int, r2::Int, c2::Int)
 
   _checkrange_or_empty(nrows(x), r1, r2) ||
   Base.throw_boundserror(x, (r1:r2, c1:c2))
