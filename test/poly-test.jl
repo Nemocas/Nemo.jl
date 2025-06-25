5eaec7692f4e36a2f63d7242134630f572dfc696
Claus Fieker <fieker@mathematik.uni-kl.de>
2025-06-25 11:33:32 +0200

    typo

--- test/poly-test.jl
+++ test/poly-test.jl
@@ -40,7 +40,7 @@ end
   p = partial_fractions(f, g)
   #order is depending on factoring, hence not deterministic
   @test Set(p) == Set([3*x + 3, 5//(x - 1), 2//(x^2 - 2*x + 1), 1//(x + 1)])
-  @set sum(p) == f//g
+  @test sum(p) == f//g
 end
 
 
