6770730f8efda1be3de31a5a35249170ec9eb1a0
John Abbott <John.Abbott@rptu.de>
2025-10-13 15:56:09 +0200

    Added is_zero_det_probabilistic also for QQMatrix

--- test/flint/fmpq_mat-test.jl
+++ test/flint/fmpq_mat-test.jl
@@ -520,6 +520,12 @@ end
   @test det(A) == 27
 end
 
+@testset "QQMatrix.is_zero_det_probabilistic" begin
+  sz = 250
+  M = matrix(QQ, sz,sz, (x->1//x).(Iterators.take(PrimesSet(2,-1), sz*sz)))
+  @time !is_zero_det_probabilistic(M)
+end
+
 @testset "QQMatrix.hilbert" begin
   S = matrix_space(QQ, 4, 4)
 
