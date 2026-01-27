10c6c710e7f6d11e5b207fcb10e4db3ea970a27c
Tobias271828 <178478878+Tobias271828@users.noreply.github.com>
2026-01-27 11:33:36 +0100

    Added min for Arbs

--- test/arb/arb-test.jl
+++ test/arb/arb-test.jl
@@ -154,7 +154,20 @@ end
     @test contains(z, max(xx, yy))
     @test contains(zz, max(xx, yy))
   end
+
+  x = RR("[1 +/- 0.5]")
+  y = RR("[0.6 +/- 0.2]")
+  z = min(x, y)
+  zz = minimum([x, y])
+  for i in 1:10
+    xx = Nemo._rand_rational_in_ball(x)
+    yy = Nemo._rand_rational_in_ball(y)
+    @test contains(z, min(xx, yy))
+    @test contains(zz, min(xx, yy))
+  end
 end
+
+
 
 @testset "ArbFieldElem.adhoc_comparison" begin
   a = RR(3)
