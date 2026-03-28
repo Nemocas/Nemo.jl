38cb2e5679fa9427ca6898e3e25c3b7ba2f1b12a
Max Horn <max@quendi.de>
2026-03-29 00:15:54 +0100

    Update src/flint/fmpz.jl

--- src/flint/fmpz.jl
+++ src/flint/fmpz.jl
@@ -1038,7 +1038,7 @@ end
 
 Return the signed remainder of $a$ mod $b$, that is, the unique integer $x$ satisfying
 $-b < 2x \le b$, possibly modifying the object $a$ in the process.
-This is a shorthand for mod_sym!(a, a, b).
+This is a shorthand for `mod_sym!(a, a, b)`.
 
 # Examples
 
