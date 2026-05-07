278efc7cb82506d7269c136ce046020c58d4ecee
Tommy Hofmann <thofma@gmail.com>
2026-05-07 10:35:10 +0200

    Wrap `fmpz_poly_mat`

---
+++ src/flint/fmpz_poly_mat.jl
@@ -0,0 +1,275 @@
+###############################################################################
+#
+#   fmpz_poly_mat.jl : FLINT matrices of polynomials over ZZ
+#
+###############################################################################
+
+###############################################################################
+#
+#   Data type and parent object methods
+#
+###############################################################################
+
+base_ring(a::ZZPolyRingMatrix) = a.base_ring
+
+#dense_matrix_type(::Type{ZZPolyRingElem}) = ZZPolyRingMatrix
+
+is_zero_initialized(::Type{ZZPolyRingMatrix}) = true
+
+###############################################################################
+#
+#   Basic manipulation
+#
+###############################################################################
+
+@inline number_of_rows(a::ZZPolyRingMatrix) = a.r
+
+@inline number_of_columns(a::ZZPolyRingMatrix) = a.c
+
+function getindex!(v::ZZPolyRingElem, a::ZZPolyRingMatrix, r::Int, c::Int)
+  @boundscheck _checkbounds(a, r, c)
+  GC.@preserve a begin
+    z = mat_entry_ptr(a, r, c)
+    set!(v, z)
+  end
+  return v
+end
+
+@inline function getindex(a::ZZPolyRingMatrix, r::Int, c::Int)
+  @boundscheck _checkbounds(a, r, c)
+  v = base_ring(a)()
+  GC.@preserve a begin
+    z = mat_entry_ptr(a, r, c)
+    set!(v, z)
+  end
+  return v
+end
+
+@inline function setindex!(a::ZZPolyRingMatrix, d::ZZPolyRingElem, r::Int, c::Int)
+  @boundscheck _checkbounds(a, r, c)
+  GC.@preserve a begin
+    z = mat_entry_ptr(a, r, c)
+    set!(z, d)
+  end
+end
+
+@inline function is_zero_entry(A::ZZPolyRingMatrix, i::Int, j::Int)
+  @boundscheck _checkbounds(A, i, j)
+  GC.@preserve A begin
+    x = mat_entry_ptr(A, i, j)
+    # fmpz_poly_is_zero is an inline C function that checks length == 0
+    # length is the 3rd field of fmpz_poly_struct (at offset 2*sizeof(Int))
+    return unsafe_load(Ptr{Int}(x), 3) == 0
+  end
+end
+
+function setindex!(a::ZZPolyRingMatrix, b::ZZPolyRingMatrix, r::UnitRange{Int64}, c::UnitRange{Int64})
+  _checkbounds(a, r, c)
+  size(b) == (length(r), length(c)) || throw(DimensionMismatch("tried to assign a $(size(b, 1))x$(size(b, 2)) matrix to a $(length(r))x$(length(c)) destination"))
+  A = view(a, r, c)
+  @ccall libflint.fmpz_poly_mat_set(A::Ref{ZZPolyRingMatrix}, b::Ref{ZZPolyRingMatrix})::Nothing
+end
+
+function deepcopy_internal(d::ZZPolyRingMatrix, dict::IdDict)
+  z = ZZPolyRingMatrix(d)
+  return z
+end
+
+function similar(x::ZZPolyRingMatrix, R::ZZPolyRing, r::Int, c::Int)
+  z = ZZPolyRingMatrix(r, c, R)
+  return z
+end
+
+###############################################################################
+#
+#   Unary operations
+#
+###############################################################################
+
+function -(x::ZZPolyRingMatrix)
+  z = similar(x)
+  neg!(z, x)
+  return z
+end
+
+###############################################################################
+#
+#   Binary operations
+#
+###############################################################################
+
+function +(x::ZZPolyRingMatrix, y::ZZPolyRingMatrix)
+  check_parent(x, y)
+  z = similar(x)
+  add!(z, x, y)
+  return z
+end
+
+function -(x::ZZPolyRingMatrix, y::ZZPolyRingMatrix)
+  check_parent(x, y)
+  z = similar(x)
+  sub!(z, x, y)
+  return z
+end
+
+function *(x::ZZPolyRingMatrix, y::ZZPolyRingMatrix)
+  @req ncols(x) == nrows(y) "Incompatible matrix dimensions"
+  if nrows(x) == ncols(y) && nrows(x) == ncols(x)
+    z = similar(x)
+  else
+    z = similar(x, nrows(x), ncols(y))
+  end
+  mul!(z, x, y)
+  return z
+end
+
+###############################################################################
+#
+#   In-place operations
+#
+###############################################################################
+
+function zero!(z::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_zero(z::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+function one!(z::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_one(z::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+function neg!(z::ZZPolyRingMatrixOrPtr, w::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_neg(z::Ref{ZZPolyRingMatrix}, w::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+function add!(z::ZZPolyRingMatrixOrPtr, x::ZZPolyRingMatrixOrPtr, y::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_add(z::Ref{ZZPolyRingMatrix}, x::Ref{ZZPolyRingMatrix}, y::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+function sub!(z::ZZPolyRingMatrixOrPtr, x::ZZPolyRingMatrixOrPtr, y::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_sub(z::Ref{ZZPolyRingMatrix}, x::Ref{ZZPolyRingMatrix}, y::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+function mul!(z::ZZPolyRingMatrixOrPtr, x::ZZPolyRingMatrixOrPtr, y::ZZPolyRingMatrixOrPtr)
+  @ccall libflint.fmpz_poly_mat_mul(z::Ref{ZZPolyRingMatrix}, x::Ref{ZZPolyRingMatrix}, y::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+###############################################################################
+#
+#   Comparison
+#
+###############################################################################
+
+function ==(x::ZZPolyRingMatrix, y::ZZPolyRingMatrix)
+  check_parent(x, y)
+  return Bool(@ccall libflint.fmpz_poly_mat_equal(x::Ref{ZZPolyRingMatrix}, y::Ref{ZZPolyRingMatrix})::Cint)
+end
+
+function iszero(a::ZZPolyRingMatrix)
+  return Bool(@ccall libflint.fmpz_poly_mat_is_zero(a::Ref{ZZPolyRingMatrix})::Cint)
+end
+
+function isone(a::ZZPolyRingMatrix)
+  return Bool(@ccall libflint.fmpz_poly_mat_is_one(a::Ref{ZZPolyRingMatrix})::Cint)
+end
+
+###############################################################################
+#
+#   Transpose
+#
+###############################################################################
+
+function transpose(a::ZZPolyRingMatrix)
+  z = similar(a, ncols(a), nrows(a))
+  @ccall libflint.fmpz_poly_mat_transpose(z::Ref{ZZPolyRingMatrix}, a::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+###############################################################################
+#
+#   Determinant
+#
+###############################################################################
+
+function det(a::ZZPolyRingMatrix)
+  @req is_square(a) "Matrix must be a square matrix"
+  z = base_ring(a)()
+  @ccall libflint.fmpz_poly_mat_det(z::Ref{ZZPolyRingElem}, a::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+###############################################################################
+#
+#   Rank
+#
+###############################################################################
+
+function rank(a::ZZPolyRingMatrix)
+  return Int(@ccall libflint.fmpz_poly_mat_rank(a::Ref{ZZPolyRingMatrix})::Int)
+end
+
+###############################################################################
+#
+#   Trace
+#
+###############################################################################
+
+function tr(a::ZZPolyRingMatrix)
+  @req is_square(a) "Matrix must be a square matrix"
+  z = base_ring(a)()
+  @ccall libflint.fmpz_poly_mat_trace(z::Ref{ZZPolyRingElem}, a::Ref{ZZPolyRingMatrix})::Nothing
+  return z
+end
+
+###############################################################################
+#
+#   Entry pointers
+#
+###############################################################################
+
+# Size of the C fmpz_poly_struct (coeffs pointer + alloc + length), without
+# the Julia-only parent field.
+const SIZEOF_FMPZ_POLY_STRUCT = 3 * sizeof(Int)
+
+function mat_entry_ptr(A::ZZPolyRingMatrix, i::Int, j::Int)
+  return Ptr{ZZPolyRingElem}(UInt(A.entries) + UInt(((i - 1) * A.stride + (j - 1)) * SIZEOF_FMPZ_POLY_STRUCT))
+end
+
+###############################################################################
+#
+#   Constructors
+#
+###############################################################################
+
+function ZZPolyRingMatrix(m::Matrix{ZZPolyRingElem})
+  r, c = size(m)
+  @req !isempty(m) "Matrix must be non-empty"
+  base_ring = parent(m[1, 1])
+  z = ZZPolyRingMatrix(r, c, base_ring)
+  GC.@preserve z for i in 1:r
+    for j in 1:c
+      el = mat_entry_ptr(z, i, j)
+      set!(el, m[i, j])
+    end
+  end
+  return z
+end
+
+function ZZPolyRingMatrix(m::MatElem{ZZPolyRingElem})
+  r = nrows(m)
+  c = ncols(m)
+  base_ring = Nemo.base_ring(m)
+  z = ZZPolyRingMatrix(r, c, base_ring)
+  GC.@preserve z for i in 1:r
+    for j in 1:c
+      el = mat_entry_ptr(z, i, j)
+      set!(el, m[i, j])
+    end
+  end
+  return z
+end
---
+++ test/flint/fmpz_poly_mat-test.jl
@@ -0,0 +1,226 @@
+@testset "ZZPolyRingMatrix.constructors" begin
+  R, x = ZZ["x"]
+
+  # Constructor from Matrix{ZZPolyRingElem}
+  m = [x+1 x; x^2 2*x+3]
+  A = ZZPolyRingMatrix(m)
+  @test isa(A, ZZPolyRingMatrix)
+  @test A isa MatElem{ZZPolyRingElem}
+  @test base_ring(A) == R
+  @test nrows(A) == 2
+  @test ncols(A) == 2
+  @test A[1, 1] == x + 1
+  @test A[1, 2] == x
+  @test A[2, 1] == x^2
+  @test A[2, 2] == 2*x + 3
+
+  # Constructor from MatElem{ZZPolyRingElem}
+  M = matrix(R, [x+1 x; x^2 2*x+3])
+  B = ZZPolyRingMatrix(M)
+  @test isa(B, ZZPolyRingMatrix)
+  @test base_ring(B) == R
+  @test nrows(B) == 2
+  @test ncols(B) == 2
+  @test B == A
+
+  # Non-square matrix
+  C = ZZPolyRingMatrix(matrix(R, [x 1 x+2; x^2+1 x-1 3]))
+  @test nrows(C) == 2
+  @test ncols(C) == 3
+
+  # 1x1 matrix
+  D = ZZPolyRingMatrix(matrix(R, reshape([x^3 + x], 1, 1)))
+  @test nrows(D) == 1
+  @test ncols(D) == 1
+  @test D[1, 1] == x^3 + x
+
+  # deepcopy preserves values and is independent
+  E = deepcopy(A)
+  @test E == A
+  E[1, 1] = R(0)
+  @test A[1, 1] == x + 1
+
+  # Constant polynomials
+  F = ZZPolyRingMatrix(matrix(R, [R(1) R(0); R(0) R(1)]))
+  @test isone(F)
+end
+
+@testset "ZZPolyRingMatrix.manipulation" begin
+  R, x = ZZ["x"]
+
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+
+  @test nrows(A) == 2
+  @test ncols(A) == 2
+  @test base_ring(A) == R
+
+  # getindex / setindex!
+  @test A[1, 1] == x + 1
+  A[2, 2] = x^3
+  @test A[2, 2] == x^3
+
+  # iszero / isone
+  Z = ZZPolyRingMatrix(zero_matrix(R, 2, 2))
+  @test iszero(Z)
+  @test !iszero(A)
+
+  I = ZZPolyRingMatrix(identity_matrix(R, 3))
+  @test isone(I)
+  @test !isone(A)
+end
+
+@testset "ZZPolyRingMatrix.similar" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+
+  S = similar(A)
+  @test S isa ZZPolyRingMatrix
+  @test nrows(S) == 2
+  @test ncols(S) == 2
+  @test base_ring(S) == R
+
+  S2 = similar(A, 3, 4)
+  @test S2 isa ZZPolyRingMatrix
+  @test nrows(S2) == 3
+  @test ncols(S2) == 4
+end
+
+@testset "ZZPolyRingMatrix.printing" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+
+  # test that default Julia printing is not used
+  @test !occursin(string(typeof(A)), string(A))
+end
+
+@testset "ZZPolyRingMatrix.unary_ops" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  B = ZZPolyRingMatrix(matrix(R, [-(x+1) -x; -x^2 -(2*x+3)]))
+
+  @test -A == B
+  @test -(-A) == A
+end
+
+@testset "ZZPolyRingMatrix.binary_ops" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  B = ZZPolyRingMatrix(matrix(R, [x 1; x+1 x^2]))
+
+  # addition
+  @test A + B == ZZPolyRingMatrix(matrix(R, [2*x+1 x+1; x^2+x+1 x^2+2*x+3]))
+
+  # subtraction
+  @test A - B == ZZPolyRingMatrix(matrix(R, [1 x-1; x^2-x-1 -x^2+2*x+3]))
+
+  # multiplication
+  prod_AB = A * B
+  @test prod_AB[1, 1] == (x+1)*x + x*(x+1)
+  @test prod_AB[1, 2] == (x+1)*1 + x*x^2
+  @test prod_AB[2, 1] == x^2*x + (2*x+3)*(x+1)
+  @test prod_AB[2, 2] == x^2*1 + (2*x+3)*x^2
+
+  # non-square multiplication
+  C = ZZPolyRingMatrix(matrix(R, [x 1 x+2; x^2+1 x-1 3]))
+  D = ZZPolyRingMatrix(matrix(R, [x 1; x^2 2; x+1 x+3]))
+  E = C * D
+  @test nrows(E) == 2
+  @test ncols(E) == 2
+
+  # identity element
+  I2 = ZZPolyRingMatrix(identity_matrix(R, 2))
+  @test A * I2 == A
+  @test I2 * A == A
+end
+
+@testset "ZZPolyRingMatrix.comparison" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  B = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  C = ZZPolyRingMatrix(matrix(R, [x 1; x+1 x^2]))
+
+  @test A == A
+  @test A == B
+  @test !(A == C)
+end
+
+@testset "ZZPolyRingMatrix.transpose" begin
+  R, x = ZZ["x"]
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  At = transpose(A)
+
+  @test nrows(At) == 2
+  @test ncols(At) == 2
+  @test At[1, 1] == A[1, 1]
+  @test At[1, 2] == A[2, 1]
+  @test At[2, 1] == A[1, 2]
+  @test At[2, 2] == A[2, 2]
+
+  # non-square transpose
+  B = ZZPolyRingMatrix(matrix(R, [x 1 x+2; x^2+1 x-1 3]))
+  Bt = transpose(B)
+  @test nrows(Bt) == 3
+  @test ncols(Bt) == 2
+  for i in 1:2, j in 1:3
+    @test Bt[j, i] == B[i, j]
+  end
+end
+
+@testset "ZZPolyRingMatrix.det" begin
+  R, x = ZZ["x"]
+
+  # 1x1
+  A = ZZPolyRingMatrix(matrix(R, reshape([x^2 + x + 1], 1, 1)))
+  @test det(A) == x^2 + x + 1
+
+  # 2x2
+  B = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  d = det(B)
+  @test d == (x+1)*(2*x+3) - x*x^2
+
+  # identity matrix has det 1
+  I3 = ZZPolyRingMatrix(identity_matrix(R, 3))
+  @test det(I3) == R(1)
+
+  # zero matrix has det 0
+  Z3 = ZZPolyRingMatrix(zero_matrix(R, 3, 3))
+  @test det(Z3) == R(0)
+
+  # non-square throws
+  C = ZZPolyRingMatrix(matrix(R, [x 1; x^2+1 x-1; R(1) x]))
+  @test_throws ArgumentError det(C)
+end
+
+@testset "ZZPolyRingMatrix.tr" begin
+  R, x = ZZ["x"]
+
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  @test tr(A) == (x+1) + (2*x+3)
+
+  I3 = ZZPolyRingMatrix(identity_matrix(R, 3))
+  @test tr(I3) == R(3)
+
+  # non-square throws
+  C = ZZPolyRingMatrix(matrix(R, [x 1 x+2; x^2+1 x-1 3]))
+  @test_throws ArgumentError tr(C)
+end
+
+@testset "ZZPolyRingMatrix.rank" begin
+  R, x = ZZ["x"]
+
+  # full rank square
+  A = ZZPolyRingMatrix(matrix(R, [x+1 x; x^2 2*x+3]))
+  @test rank(A) == 2
+
+  # rank-deficient (rows are proportional)
+  B = ZZPolyRingMatrix(matrix(R, [x 2*x; 1 2]))
+  @test rank(B) == 1
+
+  # zero matrix
+  Z = ZZPolyRingMatrix(zero_matrix(R, 2, 2))
+  @test rank(Z) == 0
+
+  # identity
+  I3 = ZZPolyRingMatrix(identity_matrix(R, 3))
+  @test rank(I3) == 3
+end
