d61571ca24214edffdd051b1637d49c1c08ab9ee
Tobias271828 <178478878+Tobias271828@users.noreply.github.com>
2026-06-02 12:32:28 +0200

    Update src/flint/fq_default_extended.jl
    Co-authored-by: Simon Brandhorst <brandhorst@math.uni-sb.de>

--- src/flint/fq_default_extended.jl
+++ src/flint/fq_default_extended.jl
@@ -700,7 +700,7 @@ function (a::FqField)(b::FqFieldElem)
   if k === a
     return b
   end
-  0 ≠ characteristic(k) ≠ characteristic(a) && error("Coercion impossible")
+   characteristic(k) != characteristic(a) && error("Coercion impossible")
   if is_absolute(a)
     da = degree(a)
     dk = degree(k)
