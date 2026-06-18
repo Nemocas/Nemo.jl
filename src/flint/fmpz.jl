601fa8835bef019b7e8a2c5d7bb981923652e79f
JohnAAbbott <124266874+JohnAAbbott@users.noreply.github.com>
2026-06-18 18:40:38 +0200

    Ensure divrem (in digits!) does RoundToZero (#2325)

--- src/flint/fmpz.jl
+++ src/flint/fmpz.jl
@@ -2539,8 +2539,7 @@ function Base.digits!(a::AbstractVector{T}, n::ZZRingElem; base::T = 10) where T
   if nbits(n)/ndigits(base, base = 2) > 100
     c = div(div(nbits(n), ndigits(base, base = 2)), 2)
     nn = ZZRingElem(base)^c
-    q, r = divrem(n, nn)
-
+    q, r = Base.divrem(n, nn, RoundToZero)
     digits!(view(a, 1:c), r, base = base)
     digits!(view(a, c+1:length(a)), q, base = base)
     return a
